98e88601e4ea380f5b2abd3fae250116209744cd
Arthur <arthurcalazans@gmail.com>
2024-12-01 01:52:08 -0300

    Serializers

--- api_rest/models.py
+++ api_rest/models.py
@@ -5,9 +5,9 @@ class Book(models.Model):
     book_id = models.IntegerField(primary_key=True)
     book_title = models.CharField(max_length=100, default='')
     book_authors = models.CharField(max_length=150, default='')
-    book_description = models.CharField(max_length=500, default='')
+    book_description = models.TextField(default='')
     book_selfLink = models.CharField(max_length=100, default='')
 
 
     def __str__(self) -> str:
-        return f'Title: {self.book_selfLink} | Authors: {self.book_authors}'
+        return f'Title: {self.book_title} | Authors: {self.book_authors}'
---
+++ api_rest/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Book
+
+# Serializar o modelo Book para JSON
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__' # api vai devolver todos os campos
