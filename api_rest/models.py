c8a8445ae18d8ebb01ff2e91ecf1e6902d26f05b
Arthur <arthurcalazans@gmail.com>
2024-12-01 01:28:37 -0300

    Modelo Livro em Models

--- api_rest/models.py
+++ api_rest/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    book_id = models.IntegerField(primary_key=True)
+    book_title = models.CharField(max_length=100, default='')
+    book_authors = models.CharField(max_length=150, default='')
+    book_description = models.CharField(max_length=500, default='')
+    book_selfLink = models.CharField(max_length=100, default='')
+
+
+    def __str__(self) -> str:
+        return f'Title: {self.book_selfLink} | Authors: {self.book_authors}'
